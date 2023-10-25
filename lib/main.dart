b7b79a1988c11089cf577976c2829f8c7ef7c43d
firmansyahswp <3337210030@untirta.ac.id>
2023-10-25 13:04:04 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,229 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+import 'package:animated_text_kit/animated_text_kit.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:confetti/confetti.dart';
+import 'package:animated_background/animated_background.dart';
+
+void main() => runApp(MyApp());
+
+class Note {
+  String title;
+  String content;
+
+  Note({required this.title, required this.content});
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: NotesScreen(),
+    );
+  }
+}
+
+class NotesScreen extends StatefulWidget {
+  @override
+  _NotesScreenState createState() => _NotesScreenState();
+}
+
+class _NotesScreenState extends State<NotesScreen> with SingleTickerProviderStateMixin {
+  List<Note> notes = [];
+  TextEditingController titleController = TextEditingController();
+  TextEditingController contentController = TextEditingController();
+  int? selectedIndex;
+  Color backgroundColor = Colors.white;
+  ConfettiController confettiController = ConfettiController();
+
+  void addNote() {
+    String title = titleController.text;
+    String content = contentController.text;
+
+    if (title.isNotEmpty && content.isNotEmpty) {
+      setState(() {
+        if (selectedIndex == null) {
+          notes.add(Note(title: title, content: content));
+        } else {
+          notes[selectedIndex!] = Note(title: title, content: content);
+          selectedIndex = null;
+        }
+        titleController.clear();
+        contentController.clear();
+        changeBackgroundColor();
+        confettiController.play();
+      });
+    }
+  }
+
+  void editNote(int index) {
+    titleController.text = notes[index].title;
+    contentController.text = notes[index].content;
+    setState(() {
+      selectedIndex = index;
+    });
+  }
+
+  void removeNote(int index) {
+    setState(() {
+      notes.removeAt(index);
+      changeBackgroundColor();
+    });
+  }
+
+  void clearNotes() {
+    setState(() {
+      notes.clear();
+      changeBackgroundColor();
+    });
+  }
+
+  void changeBackgroundColor() {
+    final random = Random();
+    final color = Color.fromARGB(
+      255,
+      random.nextInt(256),
+      random.nextInt(256),
+      random.nextInt(256),
+    );
+    setState(() {
+      backgroundColor = color;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catatan dengan Animasi"),
+      ),
+      body: AnimatedBackground(
+        behaviour: RandomParticleBehaviour(),
+        vsync: this,
+        child: Container(
+          color: backgroundColor,
+          child: Column(
+            children: <Widget>[
+              AnimatedTextKit(
+                animatedTexts: [
+                  TyperAnimatedText('Catatan Anda', textStyle: TextStyle(fontSize: 24)),
+                  TyperAnimatedText('Tambah, Edit, Hapus', textStyle: TextStyle(fontSize: 18)),
+                  FadeAnimatedText('Selamat datang di Aplikasi Catatan', textStyle: TextStyle(fontSize: 18)),
+                  ColorizeAnimatedText('Selamat Berkreasi!', textStyle: TextStyle(fontSize: 18), colors: [Colors.red, Colors.blue, Colors.green]),
+                ],
+                repeatForever: true,
+                isRepeatingAnimation: true,
+                totalRepeatCount: 100,
+                onTap: () {},
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  controller: titleController,
+                  decoration: InputDecoration(
+                    labelText: 'Judul Catatan',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    filled: true,
+                    fillColor: Colors.grey[200],
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: TextField(
+                  controller: contentController,
+                  maxLines: 4,
+                  decoration: InputDecoration(
+                    labelText: 'Isi Catatan',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    filled: true,
+                    fillColor: Colors.grey[200],
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: addNote,
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.blue,
+                  onPrimary: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  shadowColor: Colors.blue[300],
+                ),
+                child: Text(selectedIndex != null ? "Edit Catatan" : "Tambah Catatan"),
+              ),
+              SizedBox(height: 10),
+              if (notes.isNotEmpty)
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: notes.length,
+                    itemBuilder: (context, index) {
+                      return Dismissible(
+                        key: Key(notes[index].title),
+                        background: Container(
+                          color: Colors.red,
+                          child: Icon(Icons.delete, color: Colors.white),
+                          alignment: Alignment.centerLeft,
+                          padding: EdgeInsets.only(left: 16),
+                          transform: Matrix4.translationValues(10.0, 0.0, 0.0),
+                        ),
+                        onDismissed: (direction) {
+                          if (direction == DismissDirection.startToEnd) {
+                            removeNote(index);
+                          }
+                        },
+                        child: Card(
+                          elevation: 4,
+                          margin: EdgeInsets.all(8),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10),
+                          ),
+                          child: ListTile(
+                            title: Text(
+                              notes[index].title,
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            subtitle: Text(notes[index].content),
+                            trailing: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              children: [
+                                IconButton(
+                                  icon: Icon(Icons.edit),
+                                  onPressed: () {
+                                    editNote(index);
+                                  },
+                                ),
+                                IconButton(
+                                  icon: Icon(Icons.delete),
+                                  onPressed: () {
+                                    removeNote(index);
+                                  },
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+            ],
+          ),
+        ),
+      ),
+      floatingActionButton: ConfettiWidget(
+        confettiController: confettiController,
+        blastDirectionality: BlastDirectionality.explosive,
+        shouldLoop: false,
+        colors: const [Colors.green, Colors.blue, Colors.pink, Colors.orange, Colors.purple],
+      ),
+    );
+  }
+}
